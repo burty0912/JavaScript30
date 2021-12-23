a133894bfad5eaa46a6542bdb65efea229cf0aa7
Burty0912 <22586295+burty0912@users.noreply.github.com>
2021-12-23 23:30:45 +0000

    setTime function - Minute Hand

--- 02 - JS and CSS Clock/SRC/Index.js
+++ 02 - JS and CSS Clock/SRC/Index.js
@@ -1,3 +1,13 @@
 const secondHand = document.querySelector('.second-hand');
 const hourHand = document.querySelector('.hour-hand');
 const minuteHand = document.querySelector('.minute-hand');
+
+function setTime() {
+    const now = new Date();
+    const hours = now.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
+}
