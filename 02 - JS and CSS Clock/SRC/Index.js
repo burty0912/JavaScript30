8c074f14d425738fba1701028c4d1729399232d8
Burty0912 <22586295+burty0912@users.noreply.github.com>
2021-12-23 23:49:10 +0000

    setTime function - Second Hand

--- 02 - JS and CSS Clock/SRC/Index.js
+++ 02 - JS and CSS Clock/SRC/Index.js
@@ -7,9 +7,14 @@ function setTime() {
     const hours = now.getHours();
     const hoursDegrees = ((hours / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + 90;
     minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((hours / 12) * 360) + 90;
+    secondHand.style.transform = `rotate(${getSeconds}deg)`
 }
 
 setInterval(setTime, 1000)
